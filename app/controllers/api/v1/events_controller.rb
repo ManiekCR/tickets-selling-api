56a1913bc5e2f7753b42662815d4fe84942de4b3
Marian Caron <caron.marian@gmail.com>
2020-02-04 12:59:28 +0100

    add update, create and destroy methods

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::EventsController < Api::V1::BaseController
-  before_action :set_event, only: :show
+  before_action :set_event, only: [:show, :destroy]
+  before_action :event_params, only: [:update, :create]
   def index
     @events = Event.all
   end
@@ -7,9 +8,39 @@ class Api::V1::EventsController < Api::V1::BaseController
   def show
   end
 
+  def create
+    @event = Event.new(event_params)
+    if @event.save
+      render :show, status: :created
+    else
+      render_error
+    end
+  end
+
+  def update
+    if @event.update(event_params)
+      render :show
+    else
+      render_error
+    end
+  end
+
+  def destroy
+    @event.destroy
+    head :no_content
+  end
+
   private
 
   def set_event
     @event = Event.find(params[:id])
+  end
+
+  def event_params
+    params.require(:event).permit(:name, :date, :time)
+  end
+
+  def render_error
+    render json: { errors: @event.errors.full_messages }, status: :unprocessable_entity
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :events, only: [ :index, :show ] do
+      resources :events, only: [ :index, :show, :update, :create, :destroy ] do
         resources :tickets, only: :index
       end
     end
